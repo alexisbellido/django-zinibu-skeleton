65a10c96fdac74b708a8da9f53bc8b42723ba44a
Alexis Bellido <alexis@ventanazul.com>
2015-11-29 14:16:22 -0500

    almost ready to create first distribution

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
-import os
-import sys
+"""A setuptools based setup module for a skeleton Django application.
 
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+https://github.com/alexisbellido/django-zinibu-skeleton
+"""
+
+# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 from znbskeleton import get_version
 
@@ -9,8 +25,42 @@ setup(
     name='django-zinibu-skeleton',
     version=get_version().replace(' ', '-'),
     description='A skeleton Django application',
+    long_description=long_description,
+    packages=find_packages(),
+    include_package_data=True,
     author='Alexis Bellido',
     author_email='a@zinibu.com',
+    license='BSD, see LICENSE file',
     url='https://github.com/alexisbellido/django-zinibu-skeleton',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',
+
+        # Environment and framework details
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
     zip_safe=False,
 )
